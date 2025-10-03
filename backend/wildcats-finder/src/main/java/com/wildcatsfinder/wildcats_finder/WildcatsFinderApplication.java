317cd32d1d7f2deffd872205fb224206b5f0633e
Kyube <kobemarikit70@gmail.com>
2025-10-03 17:01:56 +0800

    feat: initialize frontend and backend setup

---
+++ backend/wildcats-finder/src/main/java/com/wildcatsfinder/wildcats_finder/WildcatsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.wildcatsfinder.wildcats_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcatsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcatsFinderApplication.class, args);
+	}
+
+}
---
+++ backend/wildcats-finder/src/test/java/com/wildcatsfinder/wildcats_finder/WildcatsFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcatsfinder.wildcats_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcatsFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
